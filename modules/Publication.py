e4164c35449c38f180f96189b786bf9e1cba867b
Albert Leung <1319401+alby1976@users.noreply.github.com>
2023-03-03 22:33:16 -0700

    Stub for the Publication module
    Graph representation of the data can reveal relations between
    Institution, Department, and Publication and the relationship
    between preprint and published articles.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

---
+++ modules/Publication.py
@@ -0,0 +1,37 @@
+from typing import Tuple
+
+
+class Department:
+    pass
+
+
+class Institution:
+    pass
+
+
+class Author:
+    pass
+
+
+class Article:
+    pass
+
+
+class Journal:
+    pass
+
+
+class Publication:
+    pass
+
+
+class Singleton:
+    pass
+
+
+class InstitutionPublicationMediator(metaclass=Singleton):
+    pass
+
+
+class DepartmentPublicationMediator(metaclass=Singleton):
+    pass
