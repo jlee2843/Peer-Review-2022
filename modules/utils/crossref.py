f05d51b2f825ddd9fae385f4e5ffbe4bfe18d0c3
Albert Leung <1319401+alby1976@users.noreply.github.com>
2023-03-24 08:38:15 -0600

    Implement Crossref API
    Be able to retrieve publication detail for analytics for
    prepublication and publication comparison
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- modules/utils/crossref.py
+++ modules/utils/crossref.py
@@ -1,12 +1,19 @@
 import json
 
-import crossref_commons as xref
+import crossref_commons.retrieval as xref
 
 
 def get_publication_info(doi: str) -> json:
-    xref.
-    pass
+    result = xref.get_publication_as_json(doi)
+    print(type(result))
+    print(result)
+    print(result['link'])
+    return result
 
 
 def process_publication_info(result: json):
     pass
+
+
+if __name__ == '__main__':
+    get_publication_info('10.5555/515151')
