9fc3bf21cc0fffad82090f62a28a57c279088749
Albert Leung <1319401+alby1976@users.noreply.github.com>
2023-03-24 10:31:28 -0600

    Create Crossref.py file
    Added stubs to Crossref.py so that the corresponding
    database can be queried and the objects related to
    Publications can be instantiated and the relationships
    between objects can be visualized. In addition, the
    comparisons between the initial and final version of each
    publication and be made.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- modules/utils/crossref.py
+++ modules/utils/crossref.py
@@ -1,4 +1,5 @@
 import json
+from typing import List, Dict
 
 import crossref_commons.retrieval as xref
 
@@ -11,9 +12,26 @@ def get_publication_info(doi: str) -> json:
     return result
 
 
-def process_publication_info(result: json):
+def process_publication_info(result: json) -> Dict:
+    result = {}
+    # create publication related objects
+    # get publication list
+    return result
+
+
+def process_link(links: List[Dict], key: str, value: str) -> List[str]:
+    result: Dict = {}
+    for link in links:
+        tmp = link.get(key)
+        if tmp is None:
+            continue
+        elif tmp == value:
+            result[value] = link.get('url')
     pass
 
 
 if __name__ == '__main__':
-    get_publication_info('10.5555/515151')
+    # get_publication_info('10.5555/515151')
+    # get_publication_info("10.1101/104778")
+    get_publication_info("10.1038/s41598-017-04402-4")
+    get_publication_info("10.1109/MM.2019.2910009")
