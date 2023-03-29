0296b206dffa4f87ddacbdab3487bb48fef441de
Albert Leung <1319401+alby1976@users.noreply.github.com>
2023-03-29 12:10:17 -0600

    Able to create Article object
    Added stubs to Crossref.py so that the corresponding
    database can be queried and the objects related to
    Publications can be instantiated and the relationships
    between objects can be visualized. In addition, the
    comparisons between the initial and final version of each
    publication and be made.
    
    Article is one the building blocks since it contains
    information about each publication and prepublication.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- modules/creational/factory_design_pattern.py
+++ modules/creational/factory_design_pattern.py
@@ -7,8 +7,8 @@ class Factory(metaclass=Singleton):
     _factory_map: Dict[Any, Any] = {}
     _lock: Lock = Lock()
 
-    def create_object(self, identifier: Any, class_name: str, *args, **kwargs) -> Any:
-        new_object = getattr(globals(), class_name)(True, identifier, *args, **kwargs)
+    def create_object(self, identifier: Any, class_path: str, *args, **kwargs) -> Any:
+        new_object = Factory.import_class(class_path)(True, identifier, *args, **kwargs)
 
         if self.add_object(identifier, new_object) is False:
             new_object = self.get_object(identifier)
@@ -20,7 +20,7 @@ class Factory(metaclass=Singleton):
         with self._lock:
             if self._factory_map.get(identifier) is None:
                 result = True
-                self._factory_map.update({identifier, new_object})
+                self._factory_map[identifier] = new_object
 
             return result
 
@@ -28,10 +28,18 @@ class Factory(metaclass=Singleton):
         with self._lock:
             return self._factory_map.get(identifier)
 
+    @staticmethod
+    def import_class(path: str) -> object:
+        from importlib import import_module
+        module_path, _, class_name = path.rpartition('.')
+        mod = import_module(module_path)
+        klass: object = getattr(mod, class_name)
+        return klass
+
 
 class DepartmentFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Department:
-        return super().create_object(identifier, "Department", args, kwargs)
+        return super().create_object(identifier, "modules.building_block.Department", *args, **kwargs)
 
     def get_object(self, identifier: str) -> Department:
         return super().get_object(identifier)
@@ -39,7 +47,7 @@ class DepartmentFactory(Factory):
 
 class InstitutionFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Institution:
-        return super().create_object(identifier, 'Institution', args, kwargs)
+        return super().create_object(identifier, 'modules.building_block.Institution', *args, **kwargs)
 
     def get_object(self, identifier: str) -> Institution:
         return super().get_object(identifier)
@@ -47,7 +55,7 @@ class InstitutionFactory(Factory):
 
 class AuthorFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Author:
-        return super().create_object(identifier, 'Author', args, kwargs)
+        return super().create_object(identifier, 'modules.building_block.Author', args, kwargs)
 
     def get_object(self, identifier: str) -> Author:
         return super().get_object(identifier)
@@ -55,7 +63,7 @@ class AuthorFactory(Factory):
 
 class CategoryFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Category:
-        return super().create_object(identifier, 'Category', args, kwargs)
+        return super().create_object(identifier, 'modules.building_block.Category', *args, **kwargs)
 
     def get_object(self, identifier: str) -> Category:
         return super().get_object(identifier)
@@ -63,7 +71,7 @@ class CategoryFactory(Factory):
 
 class ArticleFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Article:
-        return super().create_object(identifier=identifier, class_name='Article', *args, **kwargs)
+        return super().create_object(identifier, 'modules.building_block.Article', *args, **kwargs)
 
     def get_object(self, identifier: str) -> Article:
         return super().get_object(identifier)
@@ -71,7 +79,7 @@ class ArticleFactory(Factory):
 
 class JournalFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Journal:
-        return super().create_object(identifier, 'Journal', args, kwargs)
+        return super().create_object(identifier, 'modules.building_block.Journal', *args, **kwargs)
 
     def get_object(self, identifier: str) -> Journal:
         return super().get_object(identifier)
@@ -79,7 +87,7 @@ class JournalFactory(Factory):
 
 class PublicationFactory(Factory):
     def create_object(self, identifier: str, *args, **kwargs) -> Publication:
-        return super().create_object(identifier, 'Publication', args, kwargs)
+        return super().create_object(identifier, 'modules.building_block.Publication', *args, **kwargs)
 
     def get_object(self, identifier: str) -> Publication:
         return super().get_object(identifier)
