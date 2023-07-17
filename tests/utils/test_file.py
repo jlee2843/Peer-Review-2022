c3e5b21fdee16436051f09ab6abe9785873ec08c
Albert Leung <1319401+alby1976@users.noreply.github.com>
2023-07-17 15:21:52 -0600

    Implement ConfigObj module in file.py
    Using test-driven development this functionality was implement. With this
    implementation one is able to extract create config file that is can be
    read by ConfigObj module. With this ability one would be able to remove
    different setting from code and into a file. This functionality improves
    code maintainability and private setting from other people.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- tests/utils/test_file.py
+++ tests/utils/test_file.py
@@ -1,9 +1,11 @@
+import pytest
 from configobj import ConfigObj
 
 from modules.utils.file import create_config_file
 
 
-def test_comment():
+@pytest.fixture()
+def data_pair() -> ConfigObj:
     config1 = ConfigObj()
     config1['test'] = {'subsection': {'test1': 'test_1', 'test2': 'test_2'}, 'testing': 'testing_1'}
     config1['global'] = {'testing_A': 'A'}
@@ -13,11 +15,16 @@ def test_comment():
     tmp = config1['test']['subsection']
     getattr(tmp, 'inline_comments')['test2'] = 'testing test_2'
     getattr(config1['global'], 'inline_comments')['testing_A'] = 'testing testing_1'
-    data = {'test', {'subsection': {'test1': 'test_1', 'test2': 'test_2'}, 'testing': 'testing_1'},
-            'global', {'testing_A': 'A'}}
+
+    return config1
+
+
+def test_data_pair(data_pair: ConfigObj):
+    data = {'test': {'subsection': {'test1': 'test_1', 'test2': 'test_2'}, 'testing': 'testing_1'},
+            'global': {'testing_A': 'A'}}
 
     config = create_config_file(file="../../data/tmp.ini", params=data, comments={})
-    assert config == config1
+    assert config == data_pair
     # config1.filename = '../../data/testing.ini'
     # config1.write()
     # pprint(f'comment\n{config1.comments}')
@@ -27,4 +34,16 @@ def test_comment():
     # test_config = ConfigObj('../../data/testing.ini')
     # pprint(f'read in\n{test_config}')
     # pprint(test_config.comments)
-    pass
+
+
+def test_comments(data_pair: ConfigObj):
+    data = {'test': {'subsection': {'test1': 'test_1', 'test2': 'test_2'}, 'testing': 'testing_1'},
+            'global': {'testing_A': 'A'}}
+    comments = {'comments': {'test': ['this is a test for test', 'testing out the comment feature of ConfigObj']},
+                'inline_comments': {'test': {'subsection': {'test1': 'testing test_1'}},
+                                    'global': {'testing_A': 'testing testing_1'}}}
+
+    config: ConfigObj = create_config_file(file="../../data/tmp.ini", params=data, comments=comments)
+
+    assert config.comments == data_pair.comments
+    assert config.inline_comments == data_pair.inline_comments
