4238c470d137b20ac30e439efb8aa2d85d44824f
alby1976 <1319401+alby1976@users.noreply.github.com>
2024-09-12 16:03:27 -0600

    Refactor PathManager for thread safety
    Replaced Singleton pattern with a custom implementation using read-write locks from `readerwriterlock` to ensure thread safety. Updated methods to acquire appropriate locks during read and write operations to prevent race conditions.

--- modules/utils/file/PathManager.py
+++ modules/utils/file/PathManager.py
@@ -2,28 +2,42 @@ from datetime import datetime, timezone
 from pathlib import Path
 from typing import Union
 
-from modules.building_block import Singleton
+from readerwriterlock import rwlock
+# TODO: Need to change to dataclass
 
 
-class PathManager(metaclass=Singleton):
+class PathManager:
+    _instance = None
+
+    @classmethod
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._lock = rwlock.RWLockFair()
+            cls._rlock = cls._lock.gen_rlock()
+            cls._wlock = cls._lock.gen_wlock()
+
+        return cls._instance
+
     def __init__(self):
         self._base_path_name: Union[str, None] = None
         self._path: Union[str, None] = None
         self._filename: Union[str, None] = None
 
     def set_base_path_name(self, base_path: str) -> None:
-        with PathManager._lock:
-            if not base_path.endswith('/'):
-                base_path = base_path + '/'
+        if not base_path.endswith('/'):
+            base_path = base_path + '/'
+        with PathManager._wlock:
             self._base_path_name = base_path
             p = Path(base_path)
             p.mkdir(parents=True, exist_ok=True)
 
     def get_base_path_name(self) -> str:
-        return self._base_path_name
+        with PathManager._rlock:
+            return self._base_path_name
 
     def set_path(self, path: str) -> None:
-        with PathManager._lock:
+        with PathManager._wlock:
             if not path.endswith('/'):
                 path = path + '/'
             self._path = path
