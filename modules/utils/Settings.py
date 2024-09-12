21eab10d1762552933c6ab6a540d9a6e9307854c
alby1976 <1319401+alby1976@users.noreply.github.com>
2024-09-12 16:04:02 -0600

    Refactor `Settings` class to use `rwlock` for thread safety.
    Replaced `Singleton` metaclass with manual implementation using `rwlock` for read-write access control. This ensures thread-safe operations and removes dependency on the `Singleton` building block.

--- modules/utils/Settings.py
+++ modules/utils/Settings.py
@@ -2,21 +2,31 @@ from threading import Lock
 from typing import Optional, Union
 
 from configobj import ConfigObj
+from readerwriterlock import rwlock
 
-from modules.building_block import Singleton
 
-
-class Settings(metaclass=Singleton):
+class Settings:
     _config_object: ConfigObj = None
+    _instance = None
+
+    @classmethod
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._lock = rwlock.RWLockFair()
+            cls._instance._rlock = cls._instance._lock.gen_rlock()
+            cls._instance._wlock = cls._instance._lock.gen_wlock()
+
+        return cls._instance
 
     def __init__(self, config: ConfigObj):
-        with Settings.lock:
+        with self._wlock:
             self._config_object = config
 
     def get_value(self, key: str, section: Optional[str] = None, subsection: Optional[str] = None) -> Union[str, None]:
         value: Union[str, None] = None
 
-        with Singleton.lock:
+        with self._rlock:
             if section is not None:
                 if subsection is not None:
                     value = self._config_object[section][subsection][key]
